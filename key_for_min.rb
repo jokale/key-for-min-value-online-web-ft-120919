88a3cfc0afca17f1df10243d98fd673cd66f0856
Joanna E K <kalemajoanna@gmail.com>
2019-12-12 22:09:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  valuea = 0 
-  keya = 0 
+  valuea = nil  
+  keya = nil
   name_hash.each do |key, value|
-    if valuea == 0 || value < keya 
-      valuea = value 
-      keya = key 
+    if valuea == nil || value < keya 
+      valuea = key  
+      keya = value 
     end 
   end 
-    keya 
+    valuea
 end 
