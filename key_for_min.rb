dcd51597f8b347731fbf664b35d4746513901e4b
Joanna E K <kalemajoanna@gmail.com>
2019-12-12 22:06:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   valuea = 0 
   keya = 0 
   name_hash.each do |key, value|
-    if valuea == nil 
+    if valuea == 0 
       puts nil 
     elsif value >= 2 
 end
